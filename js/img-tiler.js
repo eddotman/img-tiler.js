4828e635047b8eef6af148914a9b12b957f61265
Eddie Kim <eddotman@gmail.com>
2013-08-17 19:31:12 -0400

    math is a little closer...

--- js/img-tiler.js
+++ js/img-tiler.js
@@ -14,23 +14,30 @@ $(document).ready(function() {
 		var c2=document.getElementById("c2");
 		var ct2=c2.getContext("2d");
 
-		var w1 = $("#c1").width();
-		var h1 = $("#c1").height();
+		var w1 = $("#img1").width();
+		var h1 = $("#img1").height();
+		var w2 = $("#c1").width();
+		var h2 = $("#c1").height();
+
 		var rows = $("#rows").val();
 		var cols = $("#cols").val();
+		
+		var wstep1 = w1 / cols;
+		var hstep1 = h1 / rows;
 
-		var wstep = w1 / cols;
-		var hstep = h1 / rows;
+		var wstep2 = w2 / cols;
+		var hstep2 = h2 / rows;
 
 		//ct2.drawImage(img1,0,0,w,h,0,0,w,h);
 
-		for (var i = 0; i < rows; i++) {
-			for (var j = 0; j < cols; j++) {
-				ct2.drawImage(img1,i*hstep,j*wstep,(i+1)*hstep,(j+1)*hstep,i*hstep*1.1,j*wstep*1.1,(i+1)*hstep,(j+1)*hstep);
+		for (var i = 0; i < cols; i++) {
+			for (var j = 0; j < rows; j++) {
+				ct2.drawImage(img1,i*hstep1,j*wstep1,(i+1)*hstep1,(j+1)*wstep1,i*hstep2+10,j*wstep2+10,(i+1)*hstep2+10,(j+1)*wstep2+10);
 			}
 		}
 
-		$("#test").text(hstep + " " + wstep);
+		$("#test").text(hstep1 + " " + wstep1 + " ");
+		$("#test").append(hstep2 + " " + wstep2);
 
 	});
 
