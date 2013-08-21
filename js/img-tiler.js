cfa770d018bdfce05621a181817dd8ef8863f7f6
Eddie Kim <eddotman@gmail.com>
2013-08-21 16:21:44 -0400

    fixed math

--- js/img-tiler.js
+++ js/img-tiler.js
@@ -2,22 +2,24 @@ $(document).ready(function() {
 
 	var c1=document.getElementById("c1");
 	var ct1=c1.getContext("2d");
-	var img1=document.getElementById("img1");
-	ct1.drawImage(img1,0,0,500,400);
+	var img1=new Image();
 
-	$("#test").html("test1");
-
+	img1.onload = function() {
+		ct1.drawImage(img1,0,0,500,400);	
+	}
+	img1.src = "img/space.jpg";
+	
 	$("#preview").click( function(){
-
-		$("#test").html("test2");
 
 		var c2=document.getElementById("c2");
 		var ct2=c2.getContext("2d");
 
 		var w1 = $("#img1").width();
 		var h1 = $("#img1").height();
-		var w2 = $("#c1").width();
-		var h2 = $("#c1").height();
+		var w2 = $("#c2").width();
+		var h2 = $("#c2").height();
+
+		ct2.clearRect(0,0,w1,w2);
 
 		var rows = $("#rows").val();
 		var cols = $("#cols").val();
@@ -28,17 +30,13 @@ $(document).ready(function() {
 		var wstep2 = w2 / cols;
 		var hstep2 = h2 / rows;
 
-		//ct2.drawImage(img1,0,0,w,h,0,0,w,h);
+		var ofs = 5;
 
-		for (var i = 0; i < cols; i++) {
-			for (var j = 0; j < rows; j++) {
-				ct2.drawImage(img1,i*hstep1,j*wstep1,(i+1)*hstep1,(j+1)*wstep1,i*hstep2+10,j*wstep2+10,(i+1)*hstep2+10,(j+1)*wstep2+10);
+		for (var i = 0; i <= rows; i++) {
+			for (var j = 0; j <= cols; j++) {
+				ct2.drawImage(img1, i*wstep1, j*hstep1, wstep1, hstep1, i*wstep2, j*hstep2, wstep2-ofs, hstep2-ofs);
 			}
 		}
-
-		$("#test").text(hstep1 + " " + wstep1 + " ");
-		$("#test").append(hstep2 + " " + wstep2);
-
 	});
 
 });
