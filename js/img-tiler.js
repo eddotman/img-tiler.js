3a5500dab0c888c35d972dd75afcdfdc590ec3ca
Eddie Kim <eddotman@gmail.com>
2013-08-29 14:16:46 -0400

    cropping almost works

--- js/img-tiler.js
+++ js/img-tiler.js
@@ -1,13 +1,26 @@
 $(document).ready(function() {
 
-	var ct1 = new fabric.Canvas("c1", {
-		containerClass: "canvas"
+	var ct1 = new fabric.Canvas("c1");
+	var cropper = new fabric.Rect({
+		originX: "left", //coordinate base is at top-left
+		originY: "top",
+		width:ct1.getWidth()/2,
+		height:ct1.getHeight()/2,
+		top:ct1.getHeight()/4,
+		left:ct1.getWidth()/4,
+		opacity: 0.3,
+		hasRotatingPoint:false,
+		lockRotation:true,
+		//lockMovementX:true,
+		//lockMovementY:true,
+		lockUniScaling:true,
+		fill:"white"
 	});
+
+	ct1.add(cropper);
+
 	var imgSrc = $("#img").attr("src");
 
-	var w0 = ct1.getWidth();
-	var h0 = ct1.getHeight();
-	
 	ct1.setBackgroundImage(imgSrc, function() {
 	  ct1.renderAll();
 	});
@@ -16,45 +29,54 @@ $(document).ready(function() {
 
 		var ct2 = new fabric.StaticCanvas("c2");
 		ct2.clear();
+		cropper.saveState();
 
-		var w1 = $("#img").width();
-		var h1 = $("#img").height();
-		var w2 = ct2.getWidth();
-		var h2 = ct2.getHeight();
+		var w = ct2.getWidth();
+		var h = ct2.getHeight();
 
 		var rows = $("#rows").val();
 		var cols = $("#cols").val();
+
+		var wstep = w / cols;
+		var hstep = h / rows;
+
+		var ofs = 5; //spacing between tiles
 		
-		var wstep1 = w1 / cols;
-		var hstep1 = h1 / rows;
+		var imgInstClip = new fabric.Image("img");
 
-		var wstep2 = w2 / cols;
-		var hstep2 = h2 / rows;
+		//Compute relative cropping
+		var cropTop = cropper.top / ct1.getHeight(); //lol @ var name
+		var cropLeft = cropper.left / ct1.getWidth();
+		var cropW = cropper.getWidth() / ct1.getWidth();
+		var cropH = cropper.getHeight() / ct1.getHeight();
 
-		var ofs = 5;
+		var nTop = -(cropTop * ct2.getHeight() * 2);
+		var nLeft = -(cropLeft * ct2.getWidth() * 2);
+		var nHeight = ct2.getHeight() / cropH;
+		var nWidth = ct2.getWidth() / cropW;
 
-		
-		var imgInstClip = new fabric.Image(img, {
-			originX: "left",
+		$("#test").html(nTop + " " + nLeft + " " + nWidth + " " + nHeight);
+
+		imgInstClip.set({
+			originX: "left", //coordinate base is at top-left
 			originY: "top",
-			top: 0,
-			left: 0,
-			height: ct2.getHeight(),
-			width: ct2.getWidth(),
-			clipTo: function (ctx) {
+			top: nTop,
+			left: nLeft,
+			height: nHeight,
+			width: nWidth,
+			clipTo: function (ctx) { //clip image to a grid of spaced rectangles
 				for (var i = 0; i <= cols; i++) {
 					for (var j = 0; j <= rows; j++) {
-						var l = (-ct2.getWidth()/2) + (i*wstep2) + ofs/2;
-						var t = (-ct2.getHeight()/2) + (j*hstep2) + ofs/2;
-			      		ctx.rect(l, t, wstep2-ofs, hstep2-ofs);
+						var l = (-ct2.getWidth()/2) + (i*wstep) + ofs/2 + nLeft;
+						var t = (-ct2.getHeight()/2) + (j*hstep) + ofs/2 + nTop;
+						var w = (wstep-ofs);
+						var h = (hstep-ofs);
+			      		ctx.rect(l, t, w, h);
 		    		}
 				}
 			}
 		});
-		ct2.add(imgInstClip).renderAll();
-		//ct2.drawImage(img1, i*wstep1, j*hstep1, wstep1, hstep1, i*wstep2, j*hstep2, wstep2-ofs, hstep2-ofs);
-	
-		//ct2.renderAll();
-	});
 
+		ct2.add(imgInstClip).renderAll();
+	});
 });
